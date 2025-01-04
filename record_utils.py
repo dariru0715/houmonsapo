ee443ba40c32f17f6339b3f49ce09936c55c9264
dariru0715 <sou07155@gmail.com>
2025-01-04 14:15:30 +0900

    修正

--- record_utils.py
+++ record_utils.py
@@ -27,7 +27,7 @@ def file_speech_to_text(audio_file):
     return text # 認識した文字を返す
 
 # 音声認識した内容を要約する機能の設定
-OpenAI.api_key = os.environ['OPENAI_API_KEY'] # 環境変数化したAPIキーの読み込み
+openai.api_key = st.secrets["OPENAI_API_KEY"] # 環境変数化したAPIキーの読み込み
 client = OpenAI() # openAIの機能をclientに代入
 
 # chatGPTにリクエストするための関数を設定。引数には書いてほしい内容と最大文字数を指定
